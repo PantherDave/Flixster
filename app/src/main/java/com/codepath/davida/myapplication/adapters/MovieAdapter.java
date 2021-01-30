b660d58d93c11c633c250962e7f7891d296a1961
DA <DA@DESKTOP-IMUTTN3>
2021-01-30 05:07:26 -0500

    Added YouTubeView to detail screen

--- app/src/main/java/com/codepath/davida/myapplication/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/davida/myapplication/adapters/MovieAdapter.java
@@ -1,21 +1,27 @@
 package com.codepath.davida.myapplication.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.codepath.davida.myapplication.DetailActivity;
 import com.codepath.davida.myapplication.model.Movie;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.codepath.davida.myapplication.R;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -51,6 +57,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -60,6 +67,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             this.tvTitle = itemView.findViewById(R.id.tvTitle) ;
             this.tvOverview = itemView.findViewById(R.id.tvOverview) ;
             this.ivPoster = itemView.findViewById(R.id.ivPoster);
+            this.container = itemView.findViewById(R.id.container);
         }
 
         public void bind(Movie movie) {
@@ -75,6 +83,16 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
                 imageUrl = movie.getPosterPath();
 
             Glide.with(context).load(imageUrl).into(ivPoster);
+            this.tvTitle.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, movie.getTitle(), Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("title", movie.getTitle());
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/codepath/davida/myapplication/model/Movie.java
+++ app/src/main/java/com/codepath/davida/myapplication/model/Movie.java
@@ -3,21 +3,30 @@ package com.codepath.davida.myapplication.model;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
+    private int movieId;
     private String backdropPath;
     private String posterPath;
     private String title;
     private String overview;
+    private double rating;
+
+    // needed by parcel
+    public Movie() {}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -42,4 +51,8 @@ public class Movie {
     public String getOverview() {
         return overview;
     }
+
+    public double getRating() { return rating; }
+
+    public int getMovieId() { return movieId; }
 }
